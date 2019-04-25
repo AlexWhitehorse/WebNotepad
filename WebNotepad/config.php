f19df7fe9f77743c4399be95966a7cfd013c2e44
AlexWhitehorse <kazavkin@gmail.com>
2019-04-25 11:48:47 +0300

    V2.0 begin

---
+++ WebNotepad/config.php
@@ -0,0 +1,12 @@
+<?php
+
+//Полный путь к папке со скриптом 
+    const PATH_TO_INDEX = '../WebNotepad/';
+
+//Полный путь к папке с плейлистами
+    const PATH_TO_FOLDER = '../WebNotepad/Playlists/';
+
+    // const PATH_TO_
+
+
+?>
